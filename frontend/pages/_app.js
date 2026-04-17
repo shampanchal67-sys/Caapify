30036c15043e33ef10e5d2e6bc9d33f0773cc30b
NishantDJ <NishantDJ@users.noreply.github.com>
2026-04-18 02:42:15 +0530

    adds Integration

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,17 @@
 import '../styles/globals.css';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* ✅ Google AdSense Script (global) */}
+      <Script
+        strategy="afterInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2767067515142068"
+        crossOrigin="anonymous"
+      />
+
+      <Component {...pageProps} />
+    </>
+  );
 }

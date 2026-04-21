1d472f6559c4926279084edda7639194c8c6e9d0
NishantDJ <NishantDJ@users.noreply.github.com>
2026-04-21 15:31:29 +0530

    ey6rudth

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -5,11 +5,18 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        {/* Adsense */}
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2767067515142068"
           crossOrigin="anonymous"
         ></script>
+
+        {/* ✅ Impact Verification */}
+        <meta
+          name="Impact-Site-Verification"
+          content="16ee40fc-8d3b-4f9f-8174-81d70d7083d4"
+        />
       </Head>
 
       <Component {...pageProps} />
